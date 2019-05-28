169b608add652d7ca1a5402a5dff33c8aada5c92
Adrian Ernesto Radillo <adrian.radillo@gmail.com>
2019-05-28 15:57:28 -0400

    get simple tones to behave as wanted

--- tone_test.m
+++ tone_test.m
@@ -3,6 +3,18 @@ clear
        %    frequency (Hz)
        %    duration  (sec)
        %    intensity (normalized)
-t=dotsPlayableTone.makePlayableTone([400; 1; .01]);
-t.side='left'; % can't check that with broken headphones yet.
-t.play();
+t_long=dotsPlayableTone.makePlayableTone([400; .3; .01]);
+t_long.side='left'; % can't check that with broken headphones yet.
+t_long.playBlocking = true;
+t_long.prepareToPlay();
+
+t_short=dotsPlayableTone.makePlayableTone([412; .03; .01]);
+t_short.side='right'; % can't check that with broken headphones yet.
+t_short.playBlocking = true;
+t_short.prepareToPlay();
+
+for i=1:5
+    t_long.play();
+    t_short.play(); pause(0.001); t_short.play();
+    pause(.04)
+end
